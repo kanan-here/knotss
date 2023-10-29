c37ac661c1df5e3981b9228f237996a1d5863df4
kanan-here <125991917+kanan-here@users.noreply.github.com>
2023-10-29 09:16:15 +0530

    Update searchUser_controller.dart

--- lib/controller/searchUser_controller.dart
+++ lib/controller/searchUser_controller.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:untitled2/model/user.dart' ;
 import 'package:get/get.dart';
 
-class SearchController extends GetxController{
+class SearcheController extends GetxController{
   final Rx<List<myUser>> _searchUsers = Rx<List<myUser>>([]);
 
   List<myUser> get searchedUsers => _searchUsers.value;
