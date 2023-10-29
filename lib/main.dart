340bfbe1921bf4c02e94cdc1933752b015c1194f
kanan-here <125991917+kanan-here@users.noreply.github.com>
2023-10-29 08:54:43 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:untitled2/quiz.dart';
-import 'start_screen.dart';
-void main(){
-  runApp(
-   Quiz()
-  );
+import 'package:get/get.dart';
+import 'package:untitled2/constants.dart';
+import 'package:untitled2/controller/auth_controller.dart';
+import 'package:untitled2/view/screens/auth/signup_screen.dart';
+
+void main()  async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value){
+    Get.put(AuthController());
+
+  });
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'KNOTS',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+
+          scaffoldBackgroundColor: backgroundColor
+      ),
+      home: SignUpScreen(),
+    );
+  }
 }
